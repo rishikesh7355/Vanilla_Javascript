ff66d4a5376181b8ee0f7e65fea0fc80574ec064
rishikesh7355 <rishiyadav280198@gmail.com>
2025-06-30 00:31:27 +0530

    working code add and del

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -1,4 +1,29 @@
-const getNewTask = document.querySelector("taskInput");
-console.log(getNewTask);
-const addBtn = document.querySelector("btn");
-console.log(addBtn);
+const inputVal = document.getElementById("taskInput");
+const addBtn = document.getElementById("btnAdd");
+
+addBtn.addEventListener("click", function () {
+    if(inputVal.value===null){
+        alert("Please enter task");
+    }
+    addNewTask(inputVal.value);
+    inputVal.value = "";
+})
+
+function addNewTask(task) {
+    const containerTwo = document.getElementById("newTaskContainer");
+    const newDiv = document.createElement("div");
+    newDiv.textContent= task;
+    containerTwo.appendChild(newDiv);
+
+    if(newDiv.textContent !== "") {
+        const delBtn = document.createElement("button");
+        delBtn.textContent = "Delete";
+        delBtn.classList.add("delBtn");
+        delBtn.addEventListener("click", function() {
+            containerTwo.removeChild(newDiv);
+            containerTwo.removeChild(delBtn);
+        })
+        containerTwo.appendChild(delBtn);
+    }
+
+    }
