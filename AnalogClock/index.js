00f9b4ab329dc2080b7aa10b16e47e302360af1c
rishikesh7355 <rishiyadav280198@gmail.com>
2025-07-09 23:54:27 +0530

    style, logic start

---
+++ AnalogClock/index.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    const date = new Date();
+    const s= date.getSeconds();
+    const m= date.getMinutes();
+    const h = date.getHours();
+
+    const secondHand = document.getElementById('second');
+    const minHand = document.getElementById('minute');
+    const hourHand = document.getElementById('hour');
+
+    
+})
