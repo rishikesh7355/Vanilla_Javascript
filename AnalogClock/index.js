b52ba3456a08189834c23335b30f5ec68fecb9ab
rishikesh7355 <rishiyadav280198@gmail.com>
2025-07-14 23:58:50 +0530

    logic added

--- AnalogClock/index.js
+++ AnalogClock/index.js
@@ -1,12 +1,22 @@
-setInterval(() => {
+const secondHand = document.getElementById('second');
+const minHand = document.getElementById('min');
+const hourHand = document.getElementById('hour');
+
+window.addEventListener('load', () => {
+    setInterval(() => {
     const date = new Date();
     const s= date.getSeconds();
     const m= date.getMinutes();
     const h = date.getHours();
 
-    const secondHand = document.getElementById('second');
-    const minHand = document.getElementById('minute');
-    const hourHand = document.getElementById('hour');
+    let rotateH = (h%12) * 30 + (m/60) * 30;
+    let rotateM = m* 6;
+    let rotateS = s*6;
+
+    hourHand.style.transform = `rotate(${rotateH}deg)`;
+    minHand.style.transform = `rotate(${rotateM}deg)`;
+    secondHand.style.transform = `rotate(${rotateS}deg)`
 
     
+},1000);
 })
