5f7976a43e75f24ee393e3733fadd9b5231d0122
rishikesh7355 <rishiyadav280198@gmail.com>
2025-07-23 23:51:04 +0530

    completed

--- Infinite_Scroll/index.js
+++ Infinite_Scroll/index.js
@@ -1,12 +1,24 @@
 const container = document.getElementById('container');
 const url = 'https://picsum.photos/v2/list/?limit=10';
 let i=0;
+
+function load() {
+    const loadingElement = document.createElement('div');   
+    loadingElement.className = 'loading';
+    loadingElement.innerText = 'Loading...';
+    container.appendChild(loadingElement);
+}
+
 const getImage = async () => {
+    load();
     try {
         const response =await fetch(url);
-        console.log(response);
+        // console.log(response);
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
+
+        const loadingElement = document.querySelector('.loading');
+        if (loadingElement) container.removeChild(loadingElement);
         data.forEach( img => {
             const imgElement = document.createElement('img');
             imgElement.src = img.download_url;
@@ -15,6 +27,9 @@ const getImage = async () => {
             container.appendChild(imgElement);
         })
     } catch (error) {
+
+        const loadingElement = document.querySelector('.loading');
+        if (loadingElement) container.removeChild(loadingElement);
         console.error('Error fetching images:', error);
     }
 }
@@ -23,12 +38,13 @@ getImage();
 window.addEventListener('scroll', () => {
     console.log(window.innerHeight);
     // hidden not visible to us
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
+
     // offsetHeight is the height of the entire document
     // scrollHeight is the height of the content that can be scrolled
     // scrollHeight and offset height are coming same here
-    console.log(document.body.offsetHeight);
-    console.log(document.body.scrollHeight);
+    // console.log(document.body.offsetHeight);
+    // console.log(document.body.scrollHeight);
     if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
         console.log('loading ...');
         getImage();
